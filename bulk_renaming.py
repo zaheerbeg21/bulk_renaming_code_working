8b07cc8308247fd97bd63e091ba0db8f739f5e9d
Zaheer <zaheerbeg21@gmail.com>
2021-04-29 15:46:07 +0530

    Bulk file renaming python code , you just have give path

---
+++ bulk_renaming.py
@@ -0,0 +1,18 @@
+import os
+# Function to rename multiple files
+def main():
+   i = 0
+   path="F:/curve_100_aug/"
+   print(path)
+   for filename in os.listdir(path):
+      new_name ="curve_" + str(i) + ".png"
+      my_source =path + filename
+      my_dest =path + new_name
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+# Driver Code
+if __name__ == '__main__':
+   # Calling main() function
+   main()
